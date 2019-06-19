45bbcb212eb1ee5df4408e967bdfab37df794102
Juju <jessesaddress@gmail.com>
2019-06-20 07:49:40 +1000

    - Added json formatter - Removed redundant code

--- lib/api.py
+++ lib/api.py
@@ -1,7 +1,12 @@
 import requests
+import json
 
+# Target url for required API GET request
 url = "https://us.api.blizzard.com/wow/achievement/2144"
 
+# url = "https://us.api.blizzard.com/wow/guild/Barthilas/Vertigo"
+
+# Auth headers
 headers = {
     'Authorization': "Bearer USI1ja1xGXzXbSmwcbjq7RoVj1xVTKtd15",
     'User-Agent': "PostmanRuntime/7.13.0",
@@ -14,28 +19,9 @@ headers = {
     'cache-control': "no-cache"
     }
 
+# Sends request
 response = requests.request("GET", url, headers=headers)
 
-print(response.text)
-
-## Test Pull Request
-#
-#
-# url = "https://us.api.blizzard.com/wow/guild/Barthilas/Vertigo"
-#
-# querystring = {"fields":"achievements%2Cchallenge","locale":"en_US","access_token":"US9ttrQtZUr3Dso6odnN1KuobGGTVE6kVq"}
-#
-# headers = {
-#     'User-Agent': "PostmanRuntime/7.13.0",
-#     'Accept': "*/*",
-#     'Cache-Control': "no-cache",
-#     'Postman-Token': "a599c5e0-d91c-4445-a770-79588dd56d76,a12b7c76-8122-4fcd-90b8-706daa207cf3",
-#     'Host': "us.api.blizzard.com",
-#     'accept-encoding': "gzip, deflate",
-#     'Connection': "keep-alive",
-#     'cache-control': "no-cache"
-#     }
-#
-# response = requests.request("GET", url, headers=headers, params=querystring)
-#
-# print(response)
+# Reformats output into jason and prints
+parsed = json.loads(response.text)
+print(json.dumps(parsed, indent=4, sort_keys=True))
