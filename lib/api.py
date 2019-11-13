9be572c6d5a62ee0ca1ef97304eda8144a4757b7
zx6r <jessesaddress@gmail.com>
2019-11-13 21:01:25 +1100

    fff

--- lib/api.py
+++ lib/api.py
@@ -2,31 +2,35 @@ import urllib3
 import requests
 import json
 
-# Target url for required API GET request
-url = "https://us.api.blizzard.com/wow/achievement/2144"
 
-# url = "https://us.api.blizzard.com/wow/guild/Barthilas/Vertigo"
+class api():
 
 
-tok = "USG3OvplmLL2cDfoE3uzLcApdZ4VSqDLVs"
+    def get_data(self, url):
+        # Target url for required API GET request
+        self.url = "https://us.api.blizzard.com/wow/achievement/2144"
 
+        # url = "https://us.api.blizzard.com/wow/guild/Barthilas/Vertigo"
 
-# Auth headers
-headers = {
-    'Authorization': "Bearer USG3OvplmLL2cDfoE3uzLcApdZ4VSqDLVs",
-    'User-Agent': "PostmanRuntime/7.13.0",
-    'Accept': "*/*",
-    'Cache-Control': "no-cache",
-    'Postman-Token': "6b6fa52e-f233-4213-afc7-b84d3d79bcaf,9a7317ac-8127-4dc9-b4b5-91039fcf1975",
-    'Host': "us.api.blizzard.com",
-    'accept-encoding': "gzip, deflate",
-    'Connection': "keep-alive",
-    'cache-control': "no-cache"
-    }
+        token = "US22faCBq46fY7Dt2L1NsNPlqabrSNyup7"
 
-# Sends request
-response = requests.request("GET", url, headers=headers)
+        # Auth headers
+        headers = {
+            'Authorization': f"Bearer {token}",
+            'User-Agent': "PostmanRuntime/7.13.0",
+            'Accept': "*/*",
+            'Cache-Control': "no-cache",
+            'Postman-Token': "6b6fa52e-f233-4213-afc7-b84d3d79bcaf,9a7317ac-8127-4dc9-b4b5-91039fcf1975",
+            'Host': "au.api.blizzard.com",
+            'accept-encoding': "gzip, deflate",
+            'Connection': "keep-alive",
+            'cache-control': "no-cache"
+        }
 
-# Reformats output into jason and prints
-parsed = json.loads(response.text)
-print(json.dumps(parsed, indent=4, sort_keys=True))
+        # # Sends request
+        response = requests.request("GET", url, headers=headers)
+        # raw = response.text
+        parsed = json.loads(response.text)
+        pretty_dat = (json.dumps(parsed, indent=4, sort_keys=True))
+        print(pretty_dat)
+
